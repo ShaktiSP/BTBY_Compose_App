6a614820b969af1261c06dea437ff591e9cebe19
shaktiprakashdubey <shaktiprakash0207@gmail.com>
2025-08-13 16:40:54 +0530

    url change

--- app/src/main/java/com/example/composeapp/appUtils/AppConstants.kt
+++ app/src/main/java/com/example/composeapp/appUtils/AppConstants.kt
@@ -6,7 +6,7 @@ object AppConstants {
     const val DEVICE_TYPE = "A"
 
     // val MY_REGION = Regions.US_EAST_2
-    const val BASE_URL = "https://api.appdevelopmentservices.in:3016/btyb_v1/"
+    const val BASE_URL = "asdfghjkl"
     const val PHONE_NUMBER = "phone_number"
     const val COUNTRY_CODE = "country_code"
     const val PIN = "pin"
